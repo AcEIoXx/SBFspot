33732c8bf966e3e07f8733f5a1c2672aee6d63f2
AcEIoXx <73141597+AcEIoXx@users.noreply.github.com>
2021-02-11 20:54:20 +0100

    ExportSpotDataToSingleLine

--- SBFspot/Inverter.cpp
+++ SBFspot/Inverter.cpp
@@ -661,6 +661,8 @@ void Inverter::exportSpotData()
     {
         if ((m_config.CSV_Export == 1) && (m_config.nospot == 0))
             ExportSpotDataToCSV(&m_config, m_inverters);
+        if (m_config.LUA_Export == 1) // Added to have a single line csv for lua scripting
+			ExportSpotDataToSingleLine(&cfg, Inverters); 
 
         if (m_config.wsl == 1)
             ExportSpotDataToWSL(&m_config, m_inverters);
